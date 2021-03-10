dbf20a06a4f7aca7f1e2fa02c48a95f4c55c890d
github <calbest31@outlook.com>
2021-03-10 00:27:16 -0500

    folder setup and linking

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function writePassword() {
+  
+}
+
+
